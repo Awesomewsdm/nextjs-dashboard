89d127a36413b4dd6181dce0f39274c5468c0219
Kordah Wisdom <88857387+Awesomewsdm@users.noreply.github.com>
2025-01-02 05:47:54 +0000

    Refactor invoices page to restore functionality with pagination, search, and data fetching

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,4 +1,5 @@
 'use client';
+import {useDebouncedCallback} from 'use-debounce';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
@@ -8,15 +9,18 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathName = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string){
+  const handleSearch =useDebouncedCallback ((term) =>{
+    console.log(term);
+
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathName}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
