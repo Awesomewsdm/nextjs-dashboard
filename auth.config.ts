d60b7ef85b1efadb135114578c955df525f9fd48
Kordah Wisdom <88857387+Awesomewsdm@users.noreply.github.com>
2025-01-05 11:02:40 +0000

    Implement authentication with credentials provider, enhance login form error handling, and update metadata for pages

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,21 @@
 import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { User } from '@/app/lib/definitions';
+import bcrypt from 'bcrypt';
+
+async function getUser(email:string): Promise<User | undefined>{
+    try {
+        const user = await sql<User> `SELECT * FROM user WHERE email=${email}`;
+        return user.rows[0];
+    } catch (error) {
+        console.error('Failed to fetch uer:', error);
+        throw new Error("Failed to fetch user.");
+        
+    }
+}
+
  
 export const authConfig = {
   pages: {
@@ -17,5 +34,22 @@ export const authConfig = {
       return true;
     },
   },
-  providers: [], // Add providers with an empty array for now
+  providers: [Credentials({
+    async authorize(credentials){
+        const parsedCredentials = z.object({ email: z.string().email(), password: z.string().min(6) }).safeParse(credentials);
+
+        if (parsedCredentials.success) {
+            const { email, password } = parsedCredentials.data;
+            const user  = await getUser(email);
+            if (!user) return null;
+
+            const passwordMatched = await bcrypt.compare(password, user.password);
+
+            if (passwordMatched) return user;
+        }
+ 
+        console.log("Invalid Credentials");
+        return null;
+    }
+  })], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
